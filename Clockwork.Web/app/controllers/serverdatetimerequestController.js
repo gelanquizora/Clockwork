ca84cc6b579605f95676878cd88043ea1b2057ad
Gereldyn Quizora <gereldynquizora@gmail.com>
2019-04-22 22:56:35 +0800

    Implement in Web project

---
+++ Clockwork.Web/app/controllers/serverdatetimerequestController.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+    angular.module('clockworkApp').controller('serverdatetimerequestController', serverdatetimerequestController);
+    serverdatetimerequestController.$inject = ['$rootScope', '$scope', 'serverdatetimerequestService'];
+
+    function serverdatetimerequestController($rootScope, $scope, serverdatetimerequestService) { 
+        $scope.dateTimeRequests = []; 
+        
+        function getServerDateTimeRequests() {
+            serverdatetimerequestService.getServerDateTimeRequests().then(function (items) {
+                $scope.dateTimeRequests = items; 
+            });
+        }
+
+        $scope.addServerDateTimeRequest = function (timeZone) { 
+            var payload = { "timeZone": timeZone};
+            serverdatetimerequestService.addServerDateTimeRequest(payload).then(function (data) {
+                getServerDateTimeRequests();
+            });
+        };
+
+        getServerDateTimeRequests();
+    }
+})();
---
+++ Clockwork.Web/app/services/serverdatetimerequestService.js
@@ -0,0 +1,27 @@
+﻿ (function () {
+    'use strict';
+     angular.module('clockworkApp').factory('serverdatetimerequestService', ['$http', '$q', serverdatetimerequestService]);
+
+     function serverdatetimerequestService($http, $q) {
+         return {
+             getServerDateTimeRequests: getServerDateTimeRequests,
+             addServerDateTimeRequest: addServerDateTimeRequest
+         };
+
+         function getServerDateTimeRequests() {
+             return $http.get('http://localhost:56627/api/server-time-requests').then(function (success) {
+                 return success.data;
+             }, function (error) {
+                 return error;
+             });
+         }
+
+         function addServerDateTimeRequest(payload) {
+             return $http.post('http://localhost:56627/api/server-time-requests', payload).then(function (success) {
+                 return success.data;
+             }, function (error) {
+                 return error;
+             });
+         }
+     }
+})();
